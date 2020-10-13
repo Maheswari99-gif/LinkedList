74d2f37197e57d486590cf63a24467c238ab64e5
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-13 19:18:41 +0530

    MaheswariKoyyani : Create a LinkedList

---
+++ MyNode.java
@@ -0,0 +1,17 @@
+package com.capgemini.linkedlist;
+
+public class MyNode<k> {
+	private k key;
+	private MyNode next;
+	public MyNode(k key) {
+		this.key=null;
+		this.next=null;
+	}
+	public MyNode getNext() {
+		return next;
+	}
+	public void setNext(MyNode next) {
+		this.next=next;
+	}
+
+}
