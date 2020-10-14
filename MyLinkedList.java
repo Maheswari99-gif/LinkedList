26ecf2ba2d918992b9b48a67aae68a10995f31a9
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-14 12:54:58 +0530

    MaheswariKoyyani : Delete a node after a given value

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -8,11 +8,17 @@ public class MyLinkedList<k> {
 	public MyLinkedList() {
 		this.head = null;
 		this.tail = null;
+		this.size=0;
 	}
-	public void insert(INode<k> myNode,INode<k> newNode) {
-		INode<k> tempNode=myNode.getNext();
+	public int size() {
+		return size;
+	}
+
+	public void insert(INode<k> myNode, INode<k> newNode) {
+		INode<k> tempNode = myNode.getNext();
 		myNode.setNext(newNode);
 		newNode.setNext(tempNode);
+		this.size++;
 	}
 
 	public void append(INode<k> newNode) {
@@ -26,8 +32,10 @@ public class MyLinkedList<k> {
 			this.tail = newNode;
 
 		}
+		size++;
 
 	}
+
 	public void add(INode<k> newNode) {
 		if (this.head == null) {
 			this.head = newNode;
@@ -39,30 +47,36 @@ public class MyLinkedList<k> {
 			this.head = newNode;
 			this.head.setNext(temp);
 		}
+		this.size++;
 	}
-	public INode<k> pop(){
-		INode<k> tempNode =this.head;
-		this.head=head.getNext();
+
+	public INode<k> pop() {
+		INode<k> tempNode = this.head;
+		this.head = head.getNext();
+		size--;
 		return tempNode;
 	}
-	public INode<k> popLast(){
-		INode<k> tempNode=this.head;
-		while(!tempNode.getNext().equals(tail)) {
-			tempNode=tempNode.getNext();
+
+	public INode<k> popLast() {
+		INode<k> tempNode = this.head;
+		while (!tempNode.getNext().equals(tail)) {
+			tempNode = tempNode.getNext();
 		}
-		this.tail=tempNode;
-		tempNode=tempNode.getNext();
+		this.tail = tempNode;
+		tempNode = tempNode.getNext();
 		this.tail.setNext(null);
+		size--;
 		return tempNode;
-		
+
 	}
-	public boolean search(k value){
-		INode<k> node=this.head;
-		while(node!=null) {
-			if(node.getKey().equals(value)) {
+
+	public boolean search(k value) {
+		INode<k> node = this.head;
+		while (node != null) {
+			if (node.getKey().equals(value)) {
 				return true;
 			}
-			node=node.getNext();
+			node = node.getNext();
 		}
 		return false;
 	}
@@ -77,13 +91,14 @@ public class MyLinkedList<k> {
 		}
 
 	}
+
 	public void insertAfter(k value, INode<k> newNode) {
 		INode<k> node = this.head;
 		while (node != null) {
 			if (node.getKey().equals(value)) {
 				break;
 			}
-			newNode=node.getNext();
+			node = node.getNext();
 		}
 		if (node != null) {
 			INode<k> temp = node.getNext();
@@ -91,6 +106,30 @@ public class MyLinkedList<k> {
 			node.getNext().setNext(temp);
 			if (node == this.tail) {
 				this.tail = newNode;
+			}
+			size++;
+		}
+	}
+	public void deleteNode(k value) {
+		INode<k> node = this.head;
+		if (node == this.tail && node != null) {
+			if (node.getKey().equals(value)) {
+				this.head = null;
+				this.tail = null;
+				size--;
+			}
+		} else if (node != null) {
+			while (node != this.tail && !(node.getNext().getKey().equals(value))) {
+				node = node.getNext();
+			}
+			if (node != this.tail) {
+				INode<k> deleteNode = node.getNext();
+				node.setNext(deleteNode.getNext());
+				deleteNode.setNext(null);
+				if (deleteNode == this.tail) {
+					this.tail = node;
+				}
+				size--;
 			}
 		}
 	}
--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -8,13 +8,49 @@ public class MyLinkedListTest {
 	public void givenSearchValue_WhenInList_ShouldReturnTrue() {
 		MyLinkedList<Integer> list = new MyLinkedList<Integer>();
 		MyNode<Integer> node1 = new MyNode<Integer>(56);
-		//MyNode<Integer> node2 = new MyNode<Integer>(30);
-		//MyNode<Integer> node3 = new MyNode<Integer>(70);
+		MyNode<Integer> node2 = new MyNode<Integer>(30);
+		MyNode<Integer> node3 = new MyNode<Integer>(70);
 		list.append(node1);
-		//list.append(node2);
-		//list.append(node3);
+		list.append(node2);
+		list.append(node3);
 		list.printMyNodes();
-		boolean result=list.search(56);
+		boolean result=list.search(30);
+		Assert.assertTrue(result);
+}
+	@Test
+	public void onInsertingAfterGivenValue_WhenSearched_ShouldReturnTrue() {
+		MyLinkedList<Integer> list = new MyLinkedList<Integer>();
+		MyNode<Integer> node1 = new MyNode<Integer>(56);
+		MyNode<Integer> node2 = new MyNode<Integer>(30);
+		MyNode<Integer> node3 = new MyNode<Integer>(70);
+		list.append(node1);
+		list.append(node2);
+		list.append(node3);
+		MyNode<Integer> node4 = new MyNode<Integer>(40);
+		list.insertAfter(30, node4);
+		list.printMyNodes();
+		boolean result = list.search(40) && node4.getNext() == node3;
+		Assert.assertTrue(result);
+	}
+	@Test
+	public void onDeletingGivenValue_WhenSearched_ShouldReturnTrue_AndListSizeReducedByOne() {
+		MyLinkedList<Integer> list = new MyLinkedList<Integer>();
+		MyNode<Integer> node1 = new MyNode<Integer>(56);
+		MyNode<Integer> node2 = new MyNode<Integer>(30);
+		MyNode<Integer> node3 = new MyNode<Integer>(70);
+		list.append(node1);
+		list.append(node2);
+		list.append(node3);
+		MyNode<Integer> node4 = new MyNode<Integer>(40);
+		list.insertAfter(30, node4);
+		System.out.println("Before deletion: ");
+		list.printMyNodes();
+		int sizeBeforeDeletion=list.size();
+		list.deleteNode(40);
+		int sizeAfterDeletion=list.size();
+		System.out.println("After deletion: ");
+		list.printMyNodes();
+		boolean result = (list.search(40)==false) || sizeAfterDeletion==sizeBeforeDeletion-1;
 		Assert.assertTrue(result);
 	}
 
